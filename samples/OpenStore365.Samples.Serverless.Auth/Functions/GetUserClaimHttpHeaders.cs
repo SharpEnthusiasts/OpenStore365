d6e18f22eec47b8eea6bcbc6cd4bafdfa1b8637d
Marcin Burak <29354406+marcinburak@users.noreply.github.com>
2020-02-22 17:50:16 +0100

    OS-2: Added a function returning only user claims from HTTP headers.

---
+++ samples/OpenStore365.Samples.Serverless.Auth/Functions/GetUserClaimHttpHeaders.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using OpenStore365.Serverless;
+using System.Linq;
+
+namespace OpenStore365.Samples.Serverless.Auth.Functions
+{
+    public static class GetUserClaimHttpHeaders
+    {
+        [FunctionName(nameof(GetUserClaimHttpHeaders))]
+        public static IActionResult Execute(
+            [HttpTrigger(AuthorizationLevel.Anonymous, HTTP.GET, Route = "Http/Headers/UserClaims")] HttpRequest request) =>
+                new OkObjectResult(request.Headers
+                    .Where(x => x.Key.StartsWith("X-MS-CLIENT"))
+                    .ToList());
+    }
+}
